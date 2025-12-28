d6aa26e65fdb00781ed4b3cf6aebfd7d3ccbd089
Ilya <ilya.skorodko@yandex.ru>
2025-12-29 01:45:37 +0300

    update

--- src/main.js
+++ src/main.js
@@ -1,3 +1,27 @@
-import { setupPage } from "./setup";
+import { setupPage, setupData } from "./setup";
 
-setupPage();
+import { data } from "./data";
+
+document.addEventListener('DOMContentLoaded', () => {
+    const url = new URL(window.location.href);
+    const personQuery = url.searchParams.get('p');
+
+    if (!personQuery) {
+        throw new Error('Miss required query params')
+    }
+
+    const personKey = atob(personQuery);
+
+    if (!data[personKey]) {
+        throw new Error('Query params refers to unknown person');
+    }
+
+    const decoded = JSON.parse(decodeURIComponent(atob(data[personKey])));
+
+    setupData(decoded);
+    setupPage();
+
+    setTimeout(() => {
+        document.getElementById('personal-info').style.opacity = '1';
+    }, 200)
+}, { once: true })
--- src/setup.js
+++ src/setup.js
@@ -2,41 +2,68 @@ export function setupPage() {
   const details = document.getElementById('act2-details');
   const summary = document.getElementById('act2-summary');
   const dialog = document.getElementById('warning-dialog');
-  
+
   const openActButton = document.getElementById('open-act');
   const closeDialogButton = document.getElementById('cancel-dialog');
-  
+
   const WARNING_LS_KEY = 'OPEN_ACT_2';
-  
+
   summary.addEventListener('click', (e) => {
     const open = e.target.closest('details')?.open;
-  
+
     if (open) {
       return;
     }
-  
+
     const itWasOpen = localStorage.getItem(WARNING_LS_KEY);
-  
+
     if (!itWasOpen) {
       e.preventDefault()
       dialog.showModal();
     }
   });
-  
+
   function closeDialog(event) {
     const { target } = event;
     const { value } = target;
-  
+
     if (value === 'cancel') {
       return;
     }
-  
+
     if (value === 'open') {
       localStorage.setItem(WARNING_LS_KEY, 'open');
       details.open = true;
     }
   }
-  
+
   openActButton.addEventListener('click', closeDialog);
   closeDialogButton.addEventListener('click', closeDialog);
+}
+
+export const setupData = (data) => {
+  const { name, image, introduction, invitation, secondAct } = data;
+
+  const avatar = document.getElementById('avatar');
+  const nameEl = document.getElementById('name');
+  const introductionEl = document.getElementById('introduction');
+  const invitationEl = document.getElementById('invitation');
+  const verdictEl = document.getElementById('verdict');
+  const secondActEl = document.getElementById('secondAct');
+
+  avatar.src = `./src/assets/avatars/${image}.jpg`;
+  nameEl.textContent = name;
+
+  const makeParagrapshList = (array) => array.map(text => {
+    const p = document.createElement('p');
+    p.textContent = text;
+    p.classList.add('text')
+    return p;
+  })
+
+  introductionEl.append(...makeParagrapshList(introduction));
+  invitationEl.append(...makeParagrapshList(invitation));
+
+  verdictEl.textContent = secondAct.verdict;
+  secondActEl.append(...makeParagrapshList(secondAct.text))
 }
